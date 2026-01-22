6b89a297235b92e54dd0d70a2ae430cc29e0a288
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2026-01-22 18:13:51 +0100

    feat: repo/branch on `pac0 run`

---
+++ packages/pac0-cli/src/pac0/cli/lib/conf.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2026 Philippe ENTZMANN <philippe@entzmann.name>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# default repo to use on `pac0 run`
+DEFAULT_REPO = "https://github.com/paxpar-tech/PA_Communautaire"
+# default branch to use on `pac0 run`
+DEFAULT_BRANCH = "main"
+
+
+SERVICES = [
+    "01-api-gateway",
+    "02-esb-central",
+    "03-controle-formats",
+    "04-validation-metier",
+    "05-conversion-formats",
+    "06-annuaire-local",
+    "07-routage",
+    "08-transmission-fiscale",
+    "09-gestion-cycle-vie",
+]
