e4611c0e47fb4809642b4a861660fb9479bb3fe1
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2026-04-07 15:39:55 +0200

    fix: wip

--- packages/pac0-cli/src/pac0/cli/lib/conf.py
+++ packages/pac0-cli/src/pac0/cli/lib/conf.py
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+
+from typing import Literal, get_args
+
 # default repo to use on `pac0 run`
 # DEFAULT_REPO = "https://github.com/paxpar-tech/PA_Communautaire"
 DEFAULT_REPO = "https://git.pdplibre.org/Construction_PA/PA_Communautaire.git"
@@ -9,8 +12,7 @@ DEFAULT_REPO = "https://git.pdplibre.org/Construction_PA/PA_Communautaire.git"
 # default branch to use on `pac0 run`
 DEFAULT_BRANCH = "main"
 
-
-SERVICES = [
+type Brique = Literal[
     "01-api-gateway",
     "02-esb-central",
     "03-controle-formats",
@@ -22,3 +24,19 @@ SERVICES = [
     "09-gestion-cycle-vie",
     "10-stockage",
 ]
+
+# SERVICES = get_args(Brique)
+SERVICES: tuple[Brique, ...] = (
+    "01-api-gateway",
+    "02-esb-central",
+    "03-controle-formats",
+    "04-validation-metier",
+    "05-conversion-formats",
+    "06-annuaire-local",
+    "07-routage",
+    "08-transmission-fiscale",
+    "09-gestion-cycle-vie",
+    "10-stockage",
+)
+
+# Briques: tuple[Brique, ...] = get_args(Brique)
