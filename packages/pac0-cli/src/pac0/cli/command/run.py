c5dda8feb5aff15add3ca81359b1a4fa4b65b979
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2026-04-07 15:38:43 +0200

    fix: wip

--- packages/pac0-cli/src/pac0/cli/command/run.py
+++ packages/pac0-cli/src/pac0/cli/command/run.py
@@ -2,112 +2,155 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
-import subprocess
-from pathlib import Path
-
 import typer
+from pydantic import BaseModel
 
-from .. import utils
-from ..lib.conf import DEFAULT_BRANCH, DEFAULT_REPO
-from . import setup
+from ..lib.conf import DEFAULT_BRANCH, DEFAULT_REPO, SERVICES, Brique
+from ..lib.process import install_run
+from ..lib.settings import settings
 
 app = typer.Typer()
 
 
-def _run_service(
-    service: str,
-    repo_url: str,
-    branch: str,
-    tools: list[str],
+class BriqueRunDef(BaseModel):
+    envvars: dict[str, str | None]
+    cmd: list[str]
+    cwd: str | None = None
+
+
+BRIQUE_RUN_DEF: dict[Brique, BriqueRunDef] = {
+    "01-api-gateway": BriqueRunDef(
+        envvars={
+            "AWS_ACCESS_KEY_ID": settings.aws_access_key_id,
+            "AWS_SECRET_ACCESS_KEY": settings.aws_secret_access_key,
+            "NATS_URL": settings.nats_url,
+            "S3_BUCKET": settings.s3_bucket,
+            "S3_REGION": settings.s3_region,
+            "S3_URL": settings.s3_url,
+        },
+        cmd=[
+            "uv",
+            "run",
+            "fastapi",
+            "dev",
+            "--host=0.0.0.0",
+            "src/pac0/service/01-api-gateway/main.py",
+        ],
+    ),
+    # TODO: ajouter les autres services
+    "02-esb-central": BriqueRunDef(
+        envvars={},
+        cmd=["nats-server", "-V", "-js"],
+    ),
+    "03-controle-formats": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.controle_formats.main:app",
+        ],
+    ),
+    "04-validation-metier": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.validation_metier.main:app",
+        ],
+    ),
+    "05-conversion-formats": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.conversion_formats.main:app",
+        ],
+    ),
+    "06-annuaire-local": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.annuaire_local.main:app",
+        ],
+    ),
+    "07-routage": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.routage.main:app",
+        ],
+    ),
+    "08-transmission-fiscale": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.transmission_fiscale.main:app",
+        ],
+    ),
+    "09-gestion-cycle-vie": BriqueRunDef(
+        envvars={},
+        cmd=[
+            "uv",
+            "run",
+            "faststream",
+            "run",
+            "pac0.service.gestion_cycle_vie.main:app",
+        ],
+    ),
+    "10-stockage": BriqueRunDef(
+        envvars={},
+        cmd=["weed", "mini", f"-dir={settings.s3_data}"],
+    ),
+}
+
+
+@app.command(name="10", help="lance le service 10-stockage")
+@app.command(name="9", help="lance le service 09-gestion-cycle")
+@app.command(name="8", help="lance le service 08-transmission-fiscale")
+@app.command(name="7", help="lance le service 07-routage")
+@app.command(name="6", help="lance le service 06-annuaire-local")
+@app.command(name="5", help="lance le service 05-conversion-formats")
+@app.command(name="4", help="lance le service 04-validation-metier")
+@app.command(name="3", help="lance le service 03-controle-formats")
+@app.command(name="2", help="lance le service 02-esb-central")
+@app.command(name="1", help="lance le service 01-api-gateway")
+def _(
+    repo: str = DEFAULT_REPO,
+    branch: str = DEFAULT_BRANCH,
+    install_tools: bool = False,
+    install_src: bool = False,
+    ctx: typer.Context = typer.Option(None, hidden=True),
 ):
-    setup.tool(tools)
-    setup.source(repo=repo_url, branch=branch)
-    _call(service)
+    # Get the command name from the context
+    command_name = ctx.info_name or "1"
+    service = SERVICES[int(command_name) - 1]
 
-
-def _call(
-    service: str,
-    run: bool = True,
-    check: bool = False,
-):
-    # service folder: "05-conversion-formats" -> "conversion_formats"
-    service_folder = "_".join(service.split("-")[1:])
-    base_folder = utils.get_app_base_folder()
-    print(f"{base_folder=}")
-    if service == "01-api-gateway":
-        full_path = f"src/pac0/service/{service_folder}/main.py"
-        cmd = ["uv", "run", "fastapi", "dev", "--host=0.0.0.0", str(full_path)]
-    elif service == "02-esb-central":
-        cmd = ["nats-server", "-V", "-js"]
-    elif service == "10-stockage":
-        cmd = ["weed", "mini", "-dir=/data"]
-    else:
-        full_path = f"src/pac0/service/{service_folder}/main:app"
-        cmd = ["uv", "run", "faststream", "run", str(full_path)]
-
-    if service != "02-esb-central":
-        if check and not Path(full_path).exists():
-            typer.echo(f"Erreur: Le service {service} n'existe pas ({full_path})")
-            raise typer.Exit(code=1)
-
-    pac0_package_base_folder = base_folder / "packages" / "pac0"
-    typer.echo(f"Lancement du service {service}...")
-    typer.echo(f"Commande: {' '.join(cmd)}")
-    typer.echo(f"pac0_package_base_folder: {pac0_package_base_folder}")
-
-    if run:
-        subprocess.call(cmd, cwd=pac0_package_base_folder)
-
-
-@app.command(name="1", help="lance le service 01-api-gateway ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("01-api-gateway", repo, branch, ["git"])
-
-
-@app.command(name="2", help="lance le service 02-esb-central ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("02-esb-central", repo, branch, ["git", "nats- branch,server"])
-
-
-@app.command(name="3", help="lance le service 03-controle-formats ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("03-controle-formats", repo, branch, ["git"])
-
-
-@app.command(name="4", help="lance le service 04-validation-metier ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("04-validation-metier", repo, branch, ["git"])
-
-
-@app.command(name="5", help="lance le service 05-conversion-formats ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("05-conversion-formats", repo, branch, ["git"])
-
-
-@app.command(name="6", help="lance le service 06-annuaire-local ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("06-annuaire-local", repo, branch, ["git"])
-
-
-@app.command(name="7", help='lance le service 07-routage") ...')
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("07-routage", repo, branch, ["git"])
-
-
-@app.command(name="8", help="lance le service 08-transmission-fiscale ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("08-transmission-fiscale", repo, branch, ["git"])
-
-
-@app.command(name="9", help="lance le service 09-gestion-cycle ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("09-gestion-cycle-vie", repo, branch, ["git"])
-
-
-@app.command(name="10", help="lance le service 09-stockage ...")
-def _(ctx: typer.Context, repo: str = DEFAULT_REPO, branch: str = DEFAULT_BRANCH):
-    _run_service("10-stockage", repo, branch, ["git", "seeseaweedfs"])
+    install_run(
+        service=service,
+        repo_url=repo,
+        branch=branch,
+        tools=["git"],
+        install_tools=install_tools,
+        install_src=install_src,
+        envvar=BRIQUE_RUN_DEF[service].envvars,
+        cmd=BRIQUE_RUN_DEF[service].cmd,
+        cwd=BRIQUE_RUN_DEF[service].cwd,
+    )
 
 
 @app.command(name="proxy", help="lance le proxy 01-api-gateway ...")
